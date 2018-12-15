6a04226b7e509426ca6d24cbad98776b4162f782
SUWANKIM <korean.kim@hotmail.com>
2018-12-15 22:50:13 +0900

    #4

--- print_test.py
+++ print_test.py
@@ -1,6 +1,8 @@
-print("%d %d %d" %(1,2,3,))
-
-print('%s %s' % ('one', 'two'))
-print("I ate %s apples." % "five")
-print("{1} {0}".format("Hello", "World"))   #format형 출력
-
+# print("%d %d %d" %(1,2,3,))
+#
+# print('%s %s' % ('one', 'two'))
+# print("I ate %s apples." % "five")
+# print("{1} {0}".format("Hello", "World"))   #format형 출력
+#
+# print("Product: %5s, Price per unit: %.5f." % ("Apple", 5.243))
+# print("Product: %10s, Price per unit: %10.3f." % ("Apple",5.243))
