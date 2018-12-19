4885e82c9d48ce9487db2bc6bbad3bedd2f3511a
SUWANKIM <korean.kim@hotmail.com>
2018-12-19 12:24:04 +0900

    #6_파이썬 List

--- ListDataType.py
+++ ListDataType.py
@@ -18,3 +18,26 @@ print(len(color)) #리스트의 길이
 print(len(color+color2))
 color[2] = 'Yellow' #리스트 내 특정 값의 데이터 변경 가능
 print(color[2])
+total_color = color+color2 #새로운 변수에 두 리스트를 더함
+print(len(total_color))
+
+
+#append 로 리스트에 데이터 추가하기
+color.append('red') #color 리스트의 맨 뒤에 red가 추가된다
+print(color)
+
+#extend
+color.extend(color2) #extend는 뒷 리스트를 앞 리스트에 붙임
+print(color)
+
+#insert
+color.insert(0, 'suwan') #insert는 특정 주소에 데이터를 넣는 함수
+print(color)
+
+#del
+del color[0] #0번째 값을 지워라
+print(color)
+
+#remove
+color.remove('red') #'red' 라는 이름으로 데이터를 지움
+print(color)
